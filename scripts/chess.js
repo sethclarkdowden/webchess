f14eae52e5c820552f1fc1d0a4ca11a601a3a353
Seth Dowden <sethclarkdowden@gmail.com>
2012-05-27 18:24:44 -0700

    started chess web game

---
+++ scripts/chess.js
@@ -0,0 +1,93 @@
+var WHITE_KING = 100;
+var WHITE_QUEEN = 90;
+var WHITE_ROOK = 50;
+var WHITE_BISHOP = 31;
+var WHITE_KNIGHT = 30;
+var WHITE_PAWN = 10;
+
+var BLACK_KING = -100;
+var BLACK_QUEEN = -90;
+var BLACK_ROOK = -50;
+var BLACK_BISHOP = -31;
+var BLACK_KNIGHT = -30;
+var BLACK_PAWN = -10;
+
+
+var board = [[BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK],
+             [BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN],
+             [WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK]];
+
+
+function getPieceName(pieceValue){
+    switch (pieceValue) {
+        case WHITE_KING:
+            return 'WHITE_KING';
+            break;
+        case WHITE_QUEEN:
+            return 'WHITE_QUEEN';
+            break;
+        case WHITE_ROOK:
+            return 'WHITE_ROOK';
+            break;
+        case WHITE_BISHOP:
+            return 'WHITE_BISHOP';
+            break;
+        case WHITE_KNIGHT:
+            return 'WHITE_KNIGHT';
+            break;
+        case WHITE_PAWN:
+            return 'WHITE_PAWN';
+            break;
+        
+        case BLACK_KING:
+            return 'BLACK_KING';
+            break;
+        case BLACK_QUEEN:
+            return 'BLACK_QUEEN';
+            break;
+        case BLACK_ROOK:
+            return 'BLACK_ROOK';
+            break;
+        case BLACK_BISHOP:
+            return 'BLACK_BISHOP';
+            break;
+        case BLACK_KNIGHT:
+            return 'BLACK_KNIGHT';
+            break;
+        case BLACK_PAWN:
+            return 'BLACK_PAWN';
+            break;
+        
+        default:
+            return 'EMPTY';
+            break;
+    }
+}
+
+$(function(){
+
+    drawBoard(board);
+});
+
+function drawBoard(board){
+    var str = '';
+    for( var i = 0 ; i < 8 ; i++ ){
+        str += '<div class="row">';
+        for( var j = 0 ; j < 8 ; j++ ){
+            str += '<div class="column ' +
+            ( (i + j) % 2 === 0 ? 'light': 'dark') + '">' +
+            '<div class="' + getPieceName(board[i][j]) + '"></div>' +
+            '</div>';
+        }
+        str += '</div>';
+    }
+    $('#board').append(str);
+}
+Function isMoveValid(){
+	
+}
